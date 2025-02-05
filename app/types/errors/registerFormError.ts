3b481bfee610d741467f2add5305fb4128051c4a
Alexandre Blanquero <aleaxndre.blanquero00@gmail.com>
2025-02-05 12:54:45 +0100

    feat: :sparkles: add register form component

---
+++ app/types/errors/registerFormError.ts
@@ -0,0 +1,8 @@
+export default interface RegisterFormError {
+  firstName?: string;
+  lastName?: string;
+  city?: string;
+  postalCode?: string;
+  email?: string;
+  birthDate?: Date | string;
+}
---
+++ app/types/registerFormData.ts
@@ -0,0 +1,8 @@
+export default interface RegisterFormData {
+  firstName: string;
+  lastName: string;
+  city: string;
+  postalCode: string;
+  email: string;
+  birthDate: Date;
+}
