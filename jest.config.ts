0b68b405d52730cbb483473df8c2004d3baa480a
Alexandre Blanquero <aleaxndre.blanquero00@gmail.com>
2025-02-05 11:13:05 +0100

    fix: :ambulance: jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -1,23 +1,14 @@
-/**
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
 
-import type {Config} from 'jest';
+const createJestConfig = nextJest({
+  dir: "./",
+});
 
 const config: Config = {
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
-  // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
-  // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
-  transform: {
-    "^.+\\.(ts|tsx|js|jsx)$": "ts-jest",
-  },
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  transformIgnorePatterns: ["/node_modules/(?!.*\\.mjs$)"],
 };
 
-export default config;
+export default createJestConfig(config);
