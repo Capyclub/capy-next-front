dd0bd715f780373f61ec320cddc31123b34dba62
Alexandre Blanquero <aleaxndre.blanquero00@gmail.com>
2025-02-06 09:36:35 +0100

    fix: :lipstick: remove page margin

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import RegisterForm from "./components/forms/RegisterForm";
 
 export default function Home() {
   return (
-    <div className="m-16">
+    <div>
       <RegisterForm/>
     </div>
   );
